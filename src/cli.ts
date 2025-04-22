2c216b842179491d56d62f0bb799a76cc299c858
Rocktim Saikia <rocktimthedev@gmail.com>
2025-04-22 14:59:57 +0000

    Simplify the ui

--- src/cli.ts
+++ src/cli.ts
@@ -2,39 +2,54 @@
 
 import { stdout } from "node:process";
 import chalk from "chalk";
+import cliSelect from "cli-select";
 import figures from "figures";
-import inquirer from "inquirer";
 import open from "open";
 
-try {
+const ITEMS = [
+	{ name: "GitHub", value: "http://github.com/rocktimsaikia" },
+	{ name: "Website", value: "https://rocktimsaikia.dev" },
+	{ name: "Discord", value: "https://discord.gg/M3d3TwNEka" },
+	{ name: "Instagram", value: "https://instagram.com/rocktim.saikiaa" },
+	{ name: "LinkedIn", value: "https://linkedin.com/in/rocktimsaikia" },
+	{ name: "Resume", value: "https://rocktimsaikia.dev/rocktim_resume.pdf" },
+];
+
+async function main() {
 	stdout.write(
 		`\nHello ${figures.heart}\nMy name is Rocktim Saikia.\nI am a software engineer who enjoys building projects, traveling, and staying fit.\n\n`,
 	);
-	const answer = await inquirer.prompt([
-		{
-			theme: {
-				helpMode: "never",
-				prefix: "",
-			},
-			type: "list",
-			name: "social",
-			message: chalk.dim("Below are my socials and resume:"),
-			loop: false,
-			choices: [
-				{ name: "GitHub", value: "http://github.com/rocktimsaikia" },
-				{ name: "Website", value: "https://rocktimsaikia.dev" },
-				{ name: "Discord", value: "https://discord.gg/M3d3TwNEka" },
-				{ name: "Instagram", value: "https://instagram.com/rocktim.saikiaa" },
-				{ name: "LinkedIn", value: "https://linkedin.com/in/rocktimsaikia" },
-				{ name: "Resume", value: "https://rocktimsaikia.dev/rocktim_resume.pdf" },
-			],
-		},
-	]);
 
-	const selectedSocial = answer.social;
-	await open(selectedSocial);
-} catch (error) {
-	if (String(error).includes("ExitPromptError")) {
-		stdout.write("\nGoodbye!\n");
+	stdout.write(chalk.dim("My socials and resume:\n"));
+
+	let cachedIndex = 0;
+
+	while (true) {
+		const selected = await cliSelect({
+			selected: chalk.blueBright(figures.pointer),
+			unselected: " ",
+			cleanup: false,
+			defaultValue: cachedIndex,
+			values: ITEMS.map((link) => link.name),
+			valueRenderer: (value, selected) => {
+				if (selected) {
+					return chalk.blueBright(value);
+				}
+
+				return value;
+			},
+		});
+		const index = selected.id as number;
+		const targetItem = ITEMS[index];
+
+		// Cache the index.
+		cachedIndex = index;
+
+		await open(targetItem.value);
 	}
 }
+
+main().catch(() => {
+	stdout.write(chalk.greenBright(`\nGoodbye ${figures.heart}\n`));
+	process.exit(0);
+});
